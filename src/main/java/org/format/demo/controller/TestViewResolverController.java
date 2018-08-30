85aed67e9c7e392763ce014d04aa901d90a707d5
ytyht226 <ytyht226@163.com>
2018-08-30 11:27:36 +0800

    viewresolver

---
+++ src/main/java/org/format/demo/controller/TestViewResolverController.java
@@ -0,0 +1,24 @@
+package org.format.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by yanghuitao on 2018/8/30.
+ */
+@Controller
+@RequestMapping(value = "/trvc")
+public class TestViewResolverController {
+    @RequestMapping("jsp")
+    public ModelAndView jsp(ModelAndView view) {
+        view.setViewName("jsp:trvc/index");
+        return view;
+    }
+
+    @RequestMapping("/ftl")
+    public ModelAndView freemarker(ModelAndView view) {
+        view.setViewName("freemarker:trvc/index");
+        return view;
+    }
+}
